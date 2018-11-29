36529a1ae623d29d562def04346f6f083892de50
lxdemm <39598333+lxdemm@users.noreply.github.com>
2018-11-29 13:33:58 -0400

    port update

--- example.js
+++ example.js
@@ -61,4 +61,4 @@ http.createServer(function (req, res) {
     res.writeHead(404, {'Content-Type': 'text/html'});
     return res.end("404 Not Found");
   }
-}).listen(5000);
+}).listen(process.env.PORT || 80);
