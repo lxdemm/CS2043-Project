dbc940f83b3215f5e6b5d79a9fdd03214d1e5690
lxdemm <39598333+lxdemm@users.noreply.github.com>
2018-11-29 14:24:26 -0400

    removed extra stuff

--- example.js
+++ example.js
@@ -45,18 +45,6 @@ http.createServer(function (req, res) {
     });
     break;
 
-    case "/coursesFinal.html":
-    fs.readFile(homePage, function(err, data) {
-      if(err) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        return res.end("404 Not Found");
-      }
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      return res.end();
-    });
-    break;
-
     default:
     res.writeHead(404, {'Content-Type': 'text/html'});
     return res.end("404 Not Found");
