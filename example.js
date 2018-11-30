5f9ffdebe032ef4b95eb21e2e54fd7194104bb1f
lxdemm <39598333+lxdemm@users.noreply.github.com>
2018-11-30 16:17:07 -0400

    server/database

--- example.js
+++ example.js
@@ -35,7 +35,7 @@ const client = new Client({
 });
 client.connect();
 
-client.query('SELECT * FROM Student', (err, res) => {
+client.query('SELECT * FROM public."Student"', (err, res) => {
   console.log(err,res);
   client.end();
 });
